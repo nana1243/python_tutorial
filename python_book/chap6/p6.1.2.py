e884c6049446f87239ce6e34acb10cd3c263dfac
nana1243 <hieun1243@gmail.com>
2021-01-26 10:23:00 +0900

    fea) 함수지향 전략

---
+++ python_book/chap6/p6.1.2.py
@@ -0,0 +1,58 @@
+from collections import namedtuple
+
+Customer = namedtuple("Customer", "name fidelity")
+
+
+class LineItem:
+    def __init__(self, product, quantity, price):
+        self.product = product
+        self.quantity = quantity
+        self.price = price
+
+    def total(self):
+        return self.price * self.quantity
+
+
+class Order:  # context
+    def __init__(self, customer, cart, promotion=None):
+        self.customer = customer
+        self.cart = list(cart)
+        self.promotion = promotion
+
+    def total(self):
+        if not hasattr(self, "_total"):
+            self._total = sum(item.total() for item in self.cart)
+        return self._total
+
+    def due(self):
+        if self.promotion is None:
+            discount = 0
+        else:
+            discount = self.promotion.discount(self)
+        return self.total() - discount
+
+    def __repr__(self):
+        fmt = "<Order total : {.2f} due: {:.2f}>"
+        return fmt.format(self.total(), self.due())
+
+
+def fidelity_promo(order):
+    """충성도 포인트가 1000점 이상인 고객에게 전체5% 할인 적용"""
+    return order.total() * 0.05 if order.customer.fidelity >= 1000 else 0
+
+
+def bulk_item_promo(order):
+    discount = 0
+    for item in order.cart:
+        if item in order.cart:
+            if item.quaantity >= 20:
+                discount += item.total() * 0.1
+
+    return discount
+
+
+def large_order_promo(order):
+    distinct_items = {item.product for item in order.cart}
+    if len(distinct_items) >= 10:
+        return order.total() * 0.07
+    return 0
