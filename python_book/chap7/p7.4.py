ead1f2a1db2d4a7059f2311b36dc45196f2d8306
nana1243 <hieun1243@gmail.com>
2021-01-30 14:09:02 +0900

    fea) 변수범위 규칙

---
+++ python_book/chap7/p7.4.py
@@ -0,0 +1,29 @@
+def f1(a):
+    print(a)
+    print(b)
+
+
+b = 6
+f1(3)
+
+
+def f2(a):
+    print(a)
+    print(b)
+
+
+f2(5)
+
+
+b = 6
+
+
+def f3(a):
+    global b
+    print(a)
+    print(b)
+    b = 9
+
+
+f3(3)
+print(b)
